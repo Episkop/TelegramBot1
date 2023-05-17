0536c2fc89ed94bbd37e89f5d44bb2588f29045b
Episkop <sferadaisona@gmail.com>
2023-05-17 09:29:17 +0300

    Добавленны кнопки выбора под сообщением, с последующей заменой оного.

--- src/main/java/ua/student/bot/telegrambot/Servise/TelegramBot.java
+++ src/main/java/ua/student/bot/telegrambot/Servise/TelegramBot.java
@@ -6,11 +6,14 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ua.student.bot.telegrambot.Config.BotConfig;
@@ -34,12 +37,14 @@ public class TelegramBot extends TelegramLongPollingBot {
     public TelegramBot(UserRepository userRepository, BotConfig botConfig) {
         this.userRepository = userRepository;
         this.botConfig = botConfig;
+//        Создание меню
         List<BotCommand> commandList = new ArrayList<>();
         commandList.add(new BotCommand("/start", "welcoming comment"));
         commandList.add(new BotCommand("/mydata", "get your data info"));
         commandList.add(new BotCommand("/deletedata", "delete my data"));
         commandList.add(new BotCommand("/help", "info how to use bot"));
         commandList.add(new BotCommand("/setting" , "set your preferences"));
+        commandList.add(new BotCommand("/register", "Yours registration in the bot"));
         try{
             execute(new SetMyCommands(commandList,new BotCommandScopeDefault(),null));
         }catch (TelegramApiException e){
@@ -75,10 +80,78 @@ public class TelegramBot extends TelegramLongPollingBot {
                 case "/help":
                     sendMessage(chatId,HELP_TEXT);
                     break;
+                case "/register":
+                    register(chatId);
+                    break;
                 default:
                     sendMessage(chatId,"Sorry, the command was not recognized");
 
             }
+//      Проверка на то что пришол не текст ка срабатывание кнопок (непосредственно под сообщением)
+        } else if (update.hasCallbackQuery()) {
+            String callBackData = update.getCallbackQuery().getData();
+            long messageId = update.getCallbackQuery().getMessage().getMessageId();
+            long chatId = update.getCallbackQuery().getMessage().getChatId();
+
+            if(callBackData.equals("YES_BUTTON")){
+                String text = "You press YES";
+                EditMessageText message = new EditMessageText();//Меняем текст сообщения. Стого где есть вопрос и кнопки. На новый.
+                message.setChatId(chatId);
+                message.setText(text);
+                message.setMessageId((int) messageId);
+
+                try {
+                    execute(message);
+                }catch (TelegramApiException e){
+                    log.error("Error occurred: " + e.getMessage());
+                }
+            }
+
+            if(callBackData.equals("NO_BUTTON")){
+                String text = "You press NO";
+                EditMessageText message = new EditMessageText();//Меняем текст сообщения. Стого где есть вопрос и кнопки. На новый.
+                message.setChatId(chatId);
+                message.setText(text);
+                message.setMessageId((int) messageId);
+
+                try {
+                    execute(message);
+                }catch (TelegramApiException e){
+                    log.error("Error occurred: " + e.getMessage());
+                }
+            }
+        }
+    }
+//Создание кнопок непосредственно под всплывающем сообщением (ответом) от бота с вариантами выбора
+    private void register(long chatId) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText("Do you want registration?");
+
+        InlineKeyboardMarkup markupInLine = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();// Спилок списков рядов кнопок
+        List<InlineKeyboardButton> rowInLine = new ArrayList<>(); //Список кнопок для 1 ряда
+        var yesButton = new InlineKeyboardButton();//Сами кнопки ряда
+
+        yesButton.setText("YES");// Это кнопка
+        yesButton.setCallbackData("YES_BUTTON");// Это идентификатор Кнопни. Его уникальный ID. Для понимания ботом какая кнопка нажата.
+
+        var noButton = new InlineKeyboardButton();//Сами кнопки ряда
+
+        noButton.setText("NO");// Это кнопка
+        noButton.setCallbackData("NO_BUTTON");// Это идентификатор Кнопни. Его уникальный ID. Для понимания ботом какая кнопка нажата.
+
+        rowInLine.add(yesButton);// Добовляем кнопки в список
+        rowInLine.add(noButton);
+
+        rowsInLine.add(rowInLine);// список в список списков
+        markupInLine.setKeyboard(rowsInLine);
+        message.setReplyMarkup(markupInLine);
+
+        try {
+            execute(message);
+        }catch (TelegramApiException e){
+            log.error("Error occurred: " + e.getMessage());
         }
     }
 
@@ -112,7 +185,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         SendMessage message = new SendMessage();
         message.setChatId(chatId);
         message.setText(textToSend);
-
+// кнопки меню вертуальной клавиатуры
         ReplyKeyboardMarkup keyboardMarkup =new ReplyKeyboardMarkup();
         List<KeyboardRow> rowList = new ArrayList<>();
         KeyboardRow keyboardButtons = new KeyboardRow();
